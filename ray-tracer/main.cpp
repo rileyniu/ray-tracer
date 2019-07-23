be518b4aaaf98675c290d53a200b33a598b36d0a
Riley Niu <hn263@cornell.edu>
2019-07-22 23:12:58 -0400

    Initial Commit

---
+++ ray-tracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ray-tracer
+//
+//  Created by Riley Niu on 7/22/19.
+//  Copyright © 2019 Riley Niu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
