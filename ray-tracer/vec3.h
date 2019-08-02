73eb998b6f4a0a6deac4003433926cbedf76a96b
Riley Niu <hn263@cornell.edu>
2019-08-02 17:56:22 -0400

    Update vec3.h

--- ray-tracer/vec3.h
+++ ray-tracer/vec3.h
@@ -13,6 +13,7 @@
 #define vec3_h
 
 class vec3 {
+    
     float e[3];
     
 public:
@@ -30,17 +31,36 @@ public:
     inline float g() const { return e[1];}
     inline float b() const { return e[2];}
     
-    // const member function cannot modify the object on which they are called
+    /*
+     const in front of data type:
+        - returns a const vec3& object, that cannot be altered once declared
+     const after func():
+        - declares the method as a const member function. A const member function is the one
+     which does not modify any member variables of the class (unless they are marked as mutable)
+        - should be used with getters
+    */
+    
     inline float length() const {
         return sqrt(e[0]^2+e[1]^2+e[2]^2);
     }
     inline float squared_length() const {
         return e[0]^2+e[1]^2+e[2]^2;
     }
-    // const value cannot be altered once declared
     
-    // operator definitions, overload and functions
+    // operator overloading returns the address of the actual object
+    // a variable which stores the address of another variable is called a pointer
+    // vec3& means function returns a reference/address instead of value
+    inline const vec3& operator+ () const {return *this; }
+    inline vec3 operator-() const () {return vec3(-e[0], -e[1], -e[2]); }
+    inline float operator[] (int i) const {return e[i]; }
+    inline float& operator[] (int i) const { return e[i]; };
     
+    inline vec3& operator+=(const vec3 &v2);
+    inline vec3& operator-=(const vec3 &v2);
+    inline vec3& operator*=(const vec3 &v2);
+    inline vec3& operator/=(const vec3 &v2);
+    inline vec3& operator*=(const float t);
+    inline vec3& operator/=(const float t);
     
 };
 
